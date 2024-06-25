cb058df4af7276ac51a6500245182c2a418c5f13
musab.bozkurt <musab.bozkurt@ozan.com>
2024-06-25 15:46:57 +0300

    Upgrade jjwt, flywaydb and postgresql. Update logback xml files to disable unnecessary logs - reformat code

--- src/main/java/com/mb/stockexchangeservice/utils/JwtUtils.java
+++ src/main/java/com/mb/stockexchangeservice/utils/JwtUtils.java
@@ -2,7 +2,10 @@ package com.mb.stockexchangeservice.utils;
 
 import com.mb.stockexchangeservice.exception.BaseException;
 import com.mb.stockexchangeservice.exception.StockExchangeServiceErrorCode;
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
